60c2d1dd97f517b388e7e16fc9a842f55fc54645
xiaoxin179 <dgy3034507585@gmail.com>
2026-03-22 21:34:36 +0800

    简易的chatclient

---
+++ spring-ai/src/main/java/com/xiaxin/SpringAiApplication.java
@@ -0,0 +1,12 @@
+package com.xiaxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiApplication.class, args);
+    }
+}
